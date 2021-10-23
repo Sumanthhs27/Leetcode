85859f0484b93255d55802b08fc00f943c2605d6
Sumanth H S <81132020+Sumanthhs27@users.noreply.github.com>
2021-10-24 00:31:53 +0530

    Word Pattern

---
+++ Hashmap/Word Pattern.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        unordered_set <char> pat;
+        unordered_set <string> str;
+        
+        for(int i =0 i<pattern.length(); i++){
+            if(pat.count())
+        }
+        
+        
+    }
+};
