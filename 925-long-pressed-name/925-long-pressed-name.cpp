fba226b8d44ece425e5433833e31f1c9c8870712
Sumanth H S <81132020+Sumanthhs27@users.noreply.github.com>
2022-03-07 00:02:35 +0530

    Time: 3 ms (39.57%), Space: 6.3 MB (84.00%) - LeetHub

--- 925-long-pressed-name/925-long-pressed-name.cpp
+++ 925-long-pressed-name/925-long-pressed-name.cpp
@@ -2,41 +2,25 @@ class Solution {
 public:
     bool isLongPressedName(string name, string typed) {
         
-//         int n = name.length();
-//         int m = typed.length();
-//         bool ans = true;
+        int n = name.length();
+        int m = typed.length();
+     
         
-//         if(m < n) return false;
-//         if(name == typed) return true;
-//         if(name[0] != typed[0]) return false;
+        if(m < n) return false;
+        if(name == typed) return true;
+        if(name[0] != typed[0]) return false;
         
-//         int curr = 1;
-//         for(int i=1; i<m; i++){
-//             if(name[curr] == typed[i]){
-//                 curr++;
-//                 curr = min(curr,n);
-//             }
-//             else{
-//                 if(typed[i] != typed[i-1]) ans = false;
-//             }
-//         }
-        
-//         return ans;
-        
-        int curr=0;
-        for(int i=0;i<typed.size();i++){
-            if(typed[i]==name[curr]){
+        int curr = 1;
+        for(int i=1; i<m; i++){
+            if(name[curr] == typed[i]){
                 curr++;
-            }else{
-                if(i>0){
-                    if(typed[i]!=typed[i-1]){
-                        return false;
-                    }
-                }else{
-                    return false;
-                }
+            }
+            else{
+                if(typed[i] != typed[i-1]) return false;
             }
         }
-        return curr == name.size();
+        cout << name[curr];
+        return curr == n;
+        
     }
 };
