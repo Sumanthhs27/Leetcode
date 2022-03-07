25885acd0916fd5bcbf3116a3f541ce504ded4f4
Sumanth H S <81132020+Sumanthhs27@users.noreply.github.com>
2022-03-07 10:16:05 +0530

    Time: 68 ms (58.54%), Space: 19.4 MB (94.55%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
@@ -9,6 +9,7 @@ public:
         }
         return true;
     }
+    
     bool validPalindrome(string s) {
         int l = 0;
         int r = s.size() - 1;
@@ -25,49 +26,3 @@ public:
 };
 
 
-
-
-// bool isPaindrome(string s, int n){
-        
-//         for(int i=0; i<n/2; i++){
-//             if(s[i] != s[n-1-i]) return false;
-//         }
-//         return true;
-//     }
-    
-    
-//     bool validPalindrome(string s) {
-//         int n = s.length();
-//         if(isPaindrome(s,n)) return true;
-        
-//         int count = 0;
-//         int i=0;
-//         int j=n-1;
-        
-//         while(i<j){
-//             if(s[i] == s[j]){
-//                 i++;
-//                 j--;
-//             }
-//             else{
-//                 if(j-1 >= 1){
-//                     if(s[i] == s[j-1]){
-//                         i++;
-//                         j -= 2;
-//                         count++;
-//                     }
-//                     else if(s[i+1] == s[j]){
-//                         i += 2;
-//                         j--;
-//                         count++;
-//                     }
-//                     else return false;
-//                 }
-//                 else {
-//                     if(count > 0) return false;
-//                     else return true;
-//                 }
-//             }
-//         }
-//         return (count <= 1);
-        
