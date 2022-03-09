0ce3301d29aa38671ce40d8342a187e12ac725ca
Sumanth H S <81132020+Sumanthhs27@users.noreply.github.com>
2022-03-10 00:08:43 +0530

    Time: 33 ms (73.75%), Space: 24.9 MB (80.40%) - LeetHub

--- 189-rotate-array/189-rotate-array.cpp
+++ 189-rotate-array/189-rotate-array.cpp
@@ -6,16 +6,13 @@ public:
         }
     }
     
-    
     void rotate(vector<int>& nums, int k) {
         
         int n = nums.size(); 
         k = k%n;
         reverse(nums,0,n-k-1);       
         reverse(nums,n-k,n-1);
-        reverse(nums,0,n-1);
- 
-        
+        reverse(nums,0,n-1);        
     }
 };
 
