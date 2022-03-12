1061575ab7e22dff15d2744b4b5724ab9b7b41e1
Sumanth H S <81132020+Sumanthhs27@users.noreply.github.com>
2022-03-12 16:16:29 +0530

    Time: 136 ms (8.32%), Space: 109.5 MB (5.88%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    
+    // Function that returns true if we can ship all containers by using max_weight in 'd' days
+    
+    bool isPossible(int d, int max_weight, vector<int> weights){
+        
+        int n = weights.size();
+        int days = 0;
+        int sum = 0;
+        
+        for(int i=0; i<n; i++){
+            if(sum + weights[i] <= max_weight) sum += weights[i];
+            else {
+                days++;
+                sum = weights[i];
+            }
+            if(days+1 > d) return false;          
+        }
+        
+        return true;        
+    }
+    
+    
+    
+    int shipWithinDays(vector<int>& weights, int days) {
+        
+        int l = *max_element(weights.begin(), weights.end());
+        int r = 0;
+        for(auto i : weights){
+            r += i;
+        }
+        
+        cout << isPossible(days,5,weights);
+        
+        while(l<r){
+            int mid = (l+r)/2;
+            
+            if(isPossible(days,mid,weights) && isPossible(days,mid-1,weights) == false) return mid;
+            else if(isPossible(days,mid,weights) == false) l = mid+1;
+            else if(isPossible(days,mid,weights) && isPossible(days,mid-1,weights)) r = mid;
+        }
+        return l;
+    }
+};
