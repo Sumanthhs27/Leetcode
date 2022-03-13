f2411addc7c146a0474b69387d57ead03c1de15f
Sumanth H S <81132020+Sumanthhs27@users.noreply.github.com>
2022-03-13 23:45:59 +0530

    Time: 214 ms (12.06%), Space: 83.4 MB (81.44%) - LeetHub

--- 860-lemonade-change/860-lemonade-change.cpp
+++ 860-lemonade-change/860-lemonade-change.cpp
@@ -2,29 +2,21 @@ class Solution {
 public:
     bool lemonadeChange(vector<int>& bills) {
         
-        // int sum=0;
-        int toReturn = 0;
         int five=0, ten=0;
-        
-        
+            
         for(int i=0; i<bills.size();i++){
             if(bills[i] == 5) five++;
             else if(bills[i] == 10) ten++;
             
-            // sum += bills[i];
-            
-            toReturn = bills[i]-5;
-            if(toReturn == 5){
+            if(bills[i]-5 == 5){
                 if(five>0) {five--; continue;}
                 else return false;
             }
-            if(toReturn == 15){
+            if(bills[i]-5 == 15){
                 if(ten>0 && five>0) {ten--; five--;}
                 else if(five >=3) five -= 3;     
                 else return false;
             }
-            // sum -= toReturn;
-            // if(sum < 0)return false;
         }
         return true;
     }
