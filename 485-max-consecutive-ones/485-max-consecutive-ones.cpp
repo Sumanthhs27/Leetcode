6cbe73f1bad15679511fdbcbbc5b6fbb596ffcfe
Sumanth H S <81132020+Sumanthhs27@users.noreply.github.com>
2022-03-14 20:37:16 +0530

    Time: 48 ms (62.84%), Space: 36.2 MB (22.63%) - LeetHub

--- 485-max-consecutive-ones/485-max-consecutive-ones.cpp
+++ 485-max-consecutive-ones/485-max-consecutive-ones.cpp
@@ -5,14 +5,8 @@ public:
         int maxi = 0;
         int count = 0;
         for(int i=0; i<nums.size(); i++){
-            if(nums[i] == 1){
-                count++;
-                maxi = max(maxi,count);
-            } 
-            else{
-                maxi = max(maxi,count);
-                count = 0;
-            }
+            if(nums[i] == 1){count++; maxi = max(maxi,count);} 
+            else count = 0;
         }
         return maxi;
     }
