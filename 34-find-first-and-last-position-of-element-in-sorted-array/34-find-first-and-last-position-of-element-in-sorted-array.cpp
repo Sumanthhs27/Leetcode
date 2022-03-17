cad7c84c0e37a0b296728e8daa5538c770b24ede
Sumanth H S <81132020+Sumanthhs27@users.noreply.github.com>
2022-03-17 09:00:18 +0530

    Time: 8 ms (79.34%), Space: 13.8 MB (19.50%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -25,19 +25,13 @@ public:
             else l = mid+1;
         }
         if(nums[l] == target) res[0] = l;
-        
-        
-        
+               
         l=0; r = nums.size()-1;
         
         while(l<r){
             mid = (l+r)/2;
             
             if(nums[mid] == target){
-                // if(mid==l) {
-                //     res[1] = mid;
-                //     break;
-                // }
                 if(nums[mid+1] > nums[mid]){
                     res[1] = mid;
                     break;
@@ -48,11 +42,7 @@ public:
             else l = mid+1;
         }
         if(nums[r] == target) res[1] = l;
-        
-        
-        
-        
-        
+
         return res;
     }
 };
