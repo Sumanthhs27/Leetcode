4c9ed78b5a348221f6c7e5ebb416fd5311fc78ba
Sumanth H S <81132020+Sumanthhs27@users.noreply.github.com>
2022-03-17 09:25:57 +0530

    Time: 8 ms (79.34%), Space: 13.7 MB (19.50%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,48 +1,90 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int> res = {-1,-1};
+//         vector<int> res = {-1,-1};
         
-        int l=0, r=nums.size()-1;
-        int mid;
-        if(r+1 == 0) return res;
+//         int l=0, r=nums.size()-1;
+//         int mid;
+//         if(r+1 == 0) return res;
+//         if(nums.size() == 1 && nums[0]==target) return {0,0};
         
-        while(l<r){
-            mid = (l+r)/2;
+//         while(l<r){
+//             mid = (l+r)/2;
             
-            if(nums[mid] == target){
-                if(mid==l) {
-                    res[0] = mid;
-                    break;
-                }
-                else if(nums[mid-1] < nums[mid]){
-                    res[0] = mid;
-                    break;
-                }
-                else if(nums[mid-1] == nums[mid]) r=mid;
-            }
-            else if(nums[mid] > target) r=mid;
-            else l = mid+1;
-        }
-        if(nums[l] == target) res[0] = l;
-               
-        l=0; r = nums.size()-1;
+//             if(nums[mid] > target) r=mid;
+//             else if (nums[mid] < target) l = mid+1;
+//             else{
+//                 if(nums[l] < target) l++;
+//                 if(nums[r] > target) r--;
+//                 if(nums[l] == target && nums[r] == target) {res[0]=l; res[1]=r; break;}
+//             } 
+//         }
+//         return res;
         
-        while(l<r){
-            mid = (l+r)/2;
+        int left=0, right=nums.size()-1, flag=0;
+        
+        if(nums.size() == 1 && nums[0]==target) return {0,0};
+        
+        while(left < right) {
+            int mid = left + (right-left)/2;
             
-            if(nums[mid] == target){
-                if(nums[mid+1] > nums[mid]){
-                    res[1] = mid;
-                    break;
-                }
-                else if(nums[mid+1] == nums[mid]) l=mid+1;
+            if(nums[mid] < target)  left=mid+1; //left part is discarded
+            
+            else if(nums[mid] > target) right=mid;  //right part is discarded
+            
+            else if(nums[mid] == target) {  //if target found, move the end points
+                if(nums[right] > target)  right--;
+                if(nums[left] < target) left++;
+                if(nums[left]==target && nums[right]==target)   {flag=1;break;}
             }
-            else if(nums[mid] > target) r=mid;
-            else l = mid+1;
+            if(left == right && nums[left]==target) {flag=1;break;}  //if range is 1
         }
-        if(nums[r] == target) res[1] = l;
-
-        return res;
+        if(flag==1)  return {left, right};
+        return {-1,-1};
     }
 };
+
+
+//         vector<int> res = {-1,-1};
+        
+//         int l=0, r=nums.size()-1;
+//         int mid;
+//         if(r+1 == 0) return res;
+        
+//         while(l<r){
+//             mid = (l+r)/2;
+            
+//             if(nums[mid] == target){
+//                 if(mid==l) {
+//                     res[0] = mid;
+//                     break;
+//                 }
+//                 else if(nums[mid-1] < nums[mid]){
+//                     res[0] = mid;
+//                     break;
+//                 }
+//                 else if(nums[mid-1] == nums[mid]) r=mid;
+//             }
+//             else if(nums[mid] > target) r=mid;
+//             else l = mid+1;
+//         }
+//         if(nums[l] == target) res[0] = l;
+               
+//         l=0; r = nums.size()-1;
+        
+//         while(l<r){
+//             mid = (l+r)/2;
+            
+//             if(nums[mid] == target){
+//                 if(nums[mid+1] > nums[mid]){
+//                     res[1] = mid;
+//                     break;
+//                 }
+//                 else if(nums[mid+1] == nums[mid]) l=mid+1;
+//             }
+//             else if(nums[mid] > target) r=mid;
+//             else l = mid+1;
+//         }
+//         if(nums[r] == target) res[1] = l;
+
+//         return res;
