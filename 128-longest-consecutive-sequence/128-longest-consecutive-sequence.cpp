3f9ab8c5beefa5b0f11229ef7839bafddd1dff17
Sumanth H S <81132020+Sumanthhs27@users.noreply.github.com>
2022-04-09 02:50:22 +0530

    Time: 143 ms (48.45%), Space: 30.8 MB (60.37%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -34,32 +34,3 @@ public:
         return count;
     }
 };
-
-
-/*        MY SOLUTION 
-
-
-    unordered_map<int,int> m;
-        int count = 1;
-        if(nums.size() == 0) return 0;
-        for(int i=0; i<nums.size(); i++){
-            m[nums[i]] = 0;
-        }
-        
-        for(int i=0; i<nums.size(); i++){
-            int ele = nums[i];
-            int b = 1;
-            
-            if(m[ele] == 0){
-                while(m.find(ele-1) != m.end()){
-                    b++;
-                    m[ele-1] = 1;
-                    ele -= 1;            
-                }            
-                count = max(count, b);
-            }
-                      
-        }        
-        return count;
-
-*/
