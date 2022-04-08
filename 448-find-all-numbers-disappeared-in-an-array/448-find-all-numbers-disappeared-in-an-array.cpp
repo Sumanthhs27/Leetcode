c43599eb772dd53226e6266c3a973ffc1b193cec
Sumanth H S <81132020+Sumanthhs27@users.noreply.github.com>
2022-04-09 01:55:53 +0530

    Time: 76 ms (60.99%), Space: 33.5 MB (94.63%) - LeetHub

--- 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.cpp
+++ 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.cpp
@@ -1,23 +1,43 @@
 class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
-      unordered_map <int, int> mapy;
-        for(int i=0; i<nums.size(); i++){
-            mapy[i+1] = 0; 
-        }
-        
-        for(int i=0; i<nums.size(); i++){
-            mapy[nums[i]] = 1;
-        }
-        
+      
         vector<int> arr;
         
-        auto it = mapy.begin();
-        
-        while(it != mapy.end()){
-            if(it->second == 0) arr.push_back(it->first);
-            it++;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[ abs(nums[i]) - 1 ] >= 0) {
+                
+                nums[abs(nums[i])-1] = (-1)*nums[abs(nums[i])-1];
+            }
         }
+        
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i] >= 0) arr.push_back(i+1);
+        }
+        
         return arr;
+        
     }
 };
+
+
+// O(N) Time  O(N) Space  
+
+//     unordered_map <int, int> mapy;
+//         for(int i=0; i<nums.size(); i++){
+//             mapy[i+1] = 0; 
+//         }
+        
+//         for(int i=0; i<nums.size(); i++){
+//             mapy[nums[i]] = 1;
+//         }
+        
+//         vector<int> arr;
+        
+//         auto it = mapy.begin();
+        
+//         while(it != mapy.end()){
+//             if(it->second == 0) arr.push_back(it->first);
+//             it++;
+//         }
+//         return arr;
