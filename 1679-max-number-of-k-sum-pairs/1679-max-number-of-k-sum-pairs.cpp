c2d0aad73ac9a39b8fa7c9481334050918cedf18
Sumanth H S <81132020+Sumanthhs27@users.noreply.github.com>
2022-04-10 14:14:00 +0530

    Time: 216 ms (42.12%), Space: 66.4 MB (52.08%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
@@ -3,6 +3,38 @@ public:
     int maxOperations(vector<int>& nums, int k) {
         
         int n = nums.size();
+        int count = 0;
+        map<int,int> m;
+        
+        for(int i=0;i<n;i++){
+            int t = k-nums[i];
+            if(m.find(t) != m.end()){
+                auto it = m.find(t);
+                if(it->second > 0){
+                    cout << i << " ";
+                    count++;
+                    it->second--;
+                }
+                else{
+                    m[nums[i]]++;
+                }
+            }
+            else {
+                m[nums[i]]++;
+            }
+        }
+        
+        
+       return count; 
+        
+    }
+};
+
+
+/*
+    O(N log N)
+    
+        int n = nums.size();
         int l=0, r=n-1;
         int count = 0;
     
@@ -17,5 +49,5 @@ public:
             else l++;
         }
         return count;
-    }
-};
+
+*/
