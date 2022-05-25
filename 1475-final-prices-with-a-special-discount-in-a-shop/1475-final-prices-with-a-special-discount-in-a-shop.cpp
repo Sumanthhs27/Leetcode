925d7ef252adc97a786a948d6f273a15e86a4b9e
Sumanth H S <81132020+Sumanthhs27@users.noreply.github.com>
2022-05-25 10:25:55 +0530

    Time: 7 ms (59.35%), Space: 9.9 MB (53.23%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -2,28 +2,52 @@ class Solution {
 public:
     vector<int> finalPrices(vector<int>& prices) {
         stack <int> st;
-        vector<int> res;
         
         for(int i=prices.size()-1; i>=0; i--){
             if(st.empty()) {
                 st.push(prices[i]);
-                res.push_back(prices[i]);
             }
             else{
+                int t = prices[i];
                 while(!st.empty()){
                     if(st.top() <= prices[i]){
-                        res.push_back(prices[i]-st.top());
+                        prices[i] = (prices[i]-st.top());
                         break;
                     }
                     else {
                         st.pop();
                     }
-                }
-                if(st.empty()) res.push_back(prices[i]);
-                st.push(prices[i]);
+                }        
+                st.push(t);
             }
         }
-        reverse(res.begin(),res.end());
-        return res;
+       
+        return prices;
     }
 };
+
+
+// stack <int> st;
+//         vector<int> res;
+        
+//         for(int i=prices.size()-1; i>=0; i--){
+//             if(st.empty()) {
+//                 st.push(prices[i]);
+//                 res.push_back(prices[i]);
+//             }
+//             else{
+//                 while(!st.empty()){
+//                     if(st.top() <= prices[i]){
+//                         res.push_back(prices[i]-st.top());
+//                         break;
+//                     }
+//                     else {
+//                         st.pop();
+//                     }
+//                 }
+//                 if(st.empty()) res.push_back(prices[i]);
+//                 st.push(prices[i]);
+//             }
+//         }
+//         reverse(res.begin(),res.end());
+//         return res;
