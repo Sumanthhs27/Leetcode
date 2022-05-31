2112e359935bf09a22dda021351845a4b4d1772c
Sumanth H S <81132020+Sumanthhs27@users.noreply.github.com>
2022-05-31 09:57:17 +0530

    Time: 8 ms (19.10%), Space: 16 MB (15.72%) - LeetHub

---
+++ 78-subsets/78-subsets.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> res;
+    
+    void subsequences(int i, int N, vector<int> arr, vector<int>& nums){
+        
+        if(i>=N){
+            res.push_back(arr);
+            return;
+        }
+        arr.push_back(nums[i]);
+        subsequences(i+1,N,arr,nums);
+        arr.pop_back();
+        subsequences(i+1,N,arr,nums);
+        
+    }
+
+    
+    
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int> arr = {};
+        subsequences(0,nums.size(),arr,nums);
+        return res;
+    }
+};
