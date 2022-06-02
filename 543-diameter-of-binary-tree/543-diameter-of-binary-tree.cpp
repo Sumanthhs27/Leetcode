7249654a9f254f22e2e2f81a137c60b34d4c1697
Sumanth H S <81132020+Sumanthhs27@users.noreply.github.com>
2022-06-02 10:42:49 +0530

    Time: 11 ms (78.32%), Space: 20.3 MB (26.07%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
@@ -9,8 +9,7 @@ public:
         maxi = max(maxi, lh+rh);
         return max(lh,rh) + 1;
     }
-    
-    
+      
     int diameterOfBinaryTree(TreeNode* root) {
         if(!root) return 0;
         height(root);
@@ -18,17 +17,20 @@ public:
     }    
 };
 
-//     int height(TreeNode* root){
-//         if(!root) return 0;
-//         if(!root->left && !root->right) return 1;
+/*
+    int height(TreeNode* root){
+        if(!root) return 0;
+        if(!root->left && !root->right) return 1;
         
-//         return max(height(root->left),height(root->right)) + 1;
-//     }
+        return max(height(root->left),height(root->right)) + 1;
+    }
           
-//     int diameterOfBinaryTree(TreeNode* root) {
-//         if(!root) return 0;
-//         int i = height(root->left) + height(root->right);
-//         int j = diameterOfBinaryTree(root->left);
-//         int k = diameterOfBinaryTree(root->right);
-//         return max(i,max(j,k));
-//     }    
+    int diameterOfBinaryTree(TreeNode* root) {
+        if(!root) return 0;
+        int i = height(root->left) + height(root->right);
+        int j = diameterOfBinaryTree(root->left);
+        int k = diameterOfBinaryTree(root->right);
+        return max(i,max(j,k));
+    }   
+    
+*/
