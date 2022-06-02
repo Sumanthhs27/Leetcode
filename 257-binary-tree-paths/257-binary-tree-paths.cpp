aa41887913ec4beecd1811073e0b2800d5fad2ec
Sumanth H S <81132020+Sumanthhs27@users.noreply.github.com>
2022-06-02 14:04:15 +0530

    Time: 8 ms (38.63%), Space: 13.2 MB (36.80%) - LeetHub

---
+++ 257-binary-tree-paths/257-binary-tree-paths.cpp
@@ -0,0 +1,32 @@
+
+class Solution {
+public:
+    
+    void helper(TreeNode* root, string ans, vector<string>& res){
+        if(!root) return;
+        if(!root->left && !root->right){
+            ans += ("->" + to_string(root->val));
+            res.push_back(ans);
+            return;
+        }
+        ans += ("->" + to_string(root->val));
+        helper(root->left, ans, res);
+        helper(root->right, ans, res);
+    }
+    
+    
+    vector<string> binaryTreePaths(TreeNode* root) {
+        vector<string> res;
+        string ans = "";
+        if(!root->left && !root->right){
+            ans += to_string(root->val);
+            res.push_back(ans);
+            return res;
+        }     
+        ans += to_string(root->val);
+        helper(root->left,ans,res);
+        helper(root->right,ans,res);
+        return res;
+    }
+    
+};
