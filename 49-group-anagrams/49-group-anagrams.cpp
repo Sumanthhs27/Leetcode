7df8daf181958a3472dc8655ff4ee565a9cda272
Sumanth H S <81132020+Sumanthhs27@users.noreply.github.com>
2022-06-13 22:34:42 +0530

    Time: 60 ms (39.85%), Space: 20.7 MB (50.52%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>> m;
+        vector<vector<string>> res;
+        
+        for(int i=0;i<strs.size();++i){
+            string s = strs[i];
+            sort(s.begin(),s.end());
+            if(m.find(s) == m.end()) m[s] = {};
+            m[s].push_back(strs[i]);
+        }
+        for(auto i:m){
+            res.push_back(i.second);
+        }
+        return res;        
+    }
+};
+
+
+/*    O(N*M) - TC    
+
+      Idea - Each group will have same hash vector
+
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
         map <vector<int>,vector<string>> m;
         
         for(int i=0;i<strs.size();++i){
@@ -17,4 +39,5 @@ public:
         }
         return res;
     }
-};
+    
+*/
