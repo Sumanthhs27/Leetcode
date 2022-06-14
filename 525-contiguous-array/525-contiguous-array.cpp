049cd063cb186553bd469896e895fbc80e539a75
Sumanth H S <81132020+Sumanthhs27@users.noreply.github.com>
2022-06-14 20:03:21 +0530

    Time: 251 ms (19.88%), Space: 83.9 MB (28.81%) - LeetHub

--- 525-contiguous-array/525-contiguous-array.cpp
+++ 525-contiguous-array/525-contiguous-array.cpp
@@ -1,25 +1,46 @@
 class Solution {
 public:
     int findMaxLength(vector<int>& nums) {
-        
-        int len = 0, maxi = 0, prefix = 0;
-        for(int i=0; i<nums.size();++i) if(nums[i] == 0) nums[i] = -1;
-        unordered_map<int,int> m;
-        
-        for(int i=0; i<nums.size(); ++i){
-            prefix += nums[i];
-            if(prefix == 0){
-                len = i+1;
-                maxi = max(len,maxi);
-            }
-            else {
-                if(m.find(prefix) == m.end()) m[prefix]  = i;
-                else {
-                    len = i-m[prefix];
-                    maxi = max(maxi,len);
+       
+        unordered_map<int,int> mp; mp[0]=-1;
+        int sum = 0,longest_subarray = 0;
+        for(int i = 0; i < nums.size(); i++)
+        {
+            sum += nums[i] == 0 ? - 1 : 1;    
+            if(mp.find(sum) != mp.end())
+            {
+                if(longest_subarray < i - mp[sum])
+                {
+                    longest_subarray = i - mp[sum];
                 }
             }
+            else
+            {
+                mp[sum] = i;
+            }
         }
-        return maxi;               
+        return longest_subarray;
+        
+        
+        
+//         int len = 0, maxi = 0, prefix = 0;
+//         for(int i=0; i<nums.size();++i) if(nums[i] == 0) nums[i] = -1;
+//         unordered_map<int,int> m;
+        
+//         for(int i=0; i<nums.size(); ++i){
+//             prefix += nums[i];
+//             if(prefix == 0){
+//                 len = i+1;
+//                 maxi = max(len,maxi);
+//             }
+//             else {
+//                 if(m.find(prefix) == m.end()) m[prefix]  = i;
+//                 else {
+//                     len = i-m[prefix];
+//                     maxi = max(maxi,len);
+//                 }
+//             }
+//         }
+//         return maxi;               
     }
 };
