f8602921bcd7ca4348a48a2e039e2b85c471134f
Sumanth H S <81132020+Sumanthhs27@users.noreply.github.com>
2022-07-01 17:07:11 +0530

    Time: 271 ms (49.97%), Space: 96.5 MB (54.04%) - LeetHub

--- 523-continuous-subarray-sum/523-continuous-subarray-sum.cpp
+++ 523-continuous-subarray-sum/523-continuous-subarray-sum.cpp
@@ -3,7 +3,8 @@ public:
     bool checkSubarraySum(vector<int>& nums, int k) {
         int n = nums.size(), sum=0;
         unordered_map<int,int> rop; // remainderOfPrefix
-        rop[0] = -1;
+        rop[0] = -1;  // needed if we find only 1 prefix with remainder = 0
+        
         for(int i=0; i<n; ++i){
             sum += nums[i];
             int remainder = sum%k;
