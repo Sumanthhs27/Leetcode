0ab52d744dab40f6aae06a8f5fe37170f1b3fe7d
Sumanth H S <81132020+Sumanthhs27@users.noreply.github.com>
2022-07-26 19:49:11 +0530

    Time: 710 ms (24.98%), Space: 0B (100.00%) - LeetHub

--- 197-rising-temperature/197-rising-temperature.sql
+++ 197-rising-temperature/197-rising-temperature.sql
@@ -1,5 +1,13 @@
 
-SELECT today.id from weather as today
-join weather as prev
-on DATEDIFF(today.recorddate, prev.recorddate) = 1
-where today.temperature > prev.temperature;
+# SELECT today.id from weather as today
+# join weather as prev
+# on DATEDIFF(today.recorddate, prev.recorddate) = 1
+# where today.temperature > prev.temperature;
+
+
+
+SELECT today.id 
+from weather as today, weather as prev
+where   DATEDIFF(today.recorddate, prev.recorddate) = 1 
+        AND  
+        today.temperature > prev.temperature;
