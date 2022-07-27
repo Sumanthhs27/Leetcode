e85904a0273ebd790c978bea079cdb87f3150d69
Sumanth H S <81132020+Sumanthhs27@users.noreply.github.com>
2022-07-27 09:49:15 +0530

    Time: 1385 ms (19.43%), Space: 0B (100.00%) - LeetHub

---
+++ 1407-top-travellers/1407-top-travellers.sql
@@ -0,0 +1,6 @@
+SELECT name, IFNULL(sum(Rides.distance),0) as travelled_distance 
+FROM Users 
+LEFT JOIN Rides
+    ON users.id = rides.user_id
+GROUP BY rides.user_id
+ORDER BY travelled_distance DESC, name;
