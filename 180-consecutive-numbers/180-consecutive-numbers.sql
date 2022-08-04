b7fc98316435eebf25820b8baba7d38189c354da
Sumanth H S <81132020+Sumanthhs27@users.noreply.github.com>
2022-08-04 19:26:48 +0530

    Time: 499 ms (71.65%), Space: 0B (100.00%) - LeetHub

--- 180-consecutive-numbers/180-consecutive-numbers.sql
+++ 180-consecutive-numbers/180-consecutive-numbers.sql
@@ -1,3 +1,6 @@
+
+### This is for 3 consequtive numbers
+
 SELECT DISTINCT a.num as ConsecutiveNums
 FROM Logs as a
 INNER JOIN Logs as b
@@ -5,3 +8,4 @@ ON a.id + 1 = b.id
     INNER JOIN Logs as c
     ON b.id + 1 = c.id
 WHERE a.num = b.num AND b.num = c.num;
+
