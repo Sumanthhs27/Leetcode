911e3c4a7c2446f6148de01b57f52bc60133c23e
Sumanth H S <81132020+Sumanthhs27@users.noreply.github.com>
2022-08-04 23:12:33 +0530

    Time: 1184 ms (23.45%), Space: 0B (100.00%) - LeetHub

--- 184-department-highest-salary/184-department-highest-salary.sql
+++ 184-department-highest-salary/184-department-highest-salary.sql
@@ -1,12 +1,12 @@
-SELECT d.name as Department, e.name as Employee, e.salary 
-FROM Employee as e, Department as d, 
-     ( SELECT emp.departmentId as id, max(emp.salary) as max_salary 
-       FROM employee as emp
-       GROUP BY emp.departmentId 
-     ) as T
-WHERE e.departmentId = d.id AND
-      e.salary = T.max_salary AND
-      d.id = T.id
+# SELECT d.name as Department, e.name as Employee, e.salary 
+# FROM Employee as e, Department as d, 
+#      ( SELECT emp.departmentId as id, max(emp.salary) as max_salary 
+#        FROM employee as emp
+#        GROUP BY emp.departmentId 
+#      ) as T
+# WHERE e.departmentId = d.id AND
+#       e.salary = T.max_salary AND
+#       d.id = T.id
       
       
       
@@ -20,3 +20,7 @@ WHERE e.departmentId = d.id AND
 #   AND (DepartmentId,Salary) in 
 #   (SELECT DepartmentId,max(Salary) as max FROM Employee GROUP BY DepartmentId)
       
+      
+Select Department.Name Department, emp1.Name Employee, emp1.Salary from 
+Employee emp1 join Department on emp1.DepartmentId = Department.Id
+where emp1.Salary = (Select Max(Salary) from Employee emp2 where emp2.DepartmentId = emp1.DepartmentId);
