523d0c417af9cc44ec634ccabb121f2ae8fe18ae
Sumanth H S <81132020+Sumanthhs27@users.noreply.github.com>
2022-08-04 23:08:42 +0530

    Time: 671 ms (61.77%), Space: 0B (100.00%) - LeetHub

--- 184-department-highest-salary/184-department-highest-salary.sql
+++ 184-department-highest-salary/184-department-highest-salary.sql
@@ -10,5 +10,13 @@ WHERE e.departmentId = d.id AND
       
       
       
+# ### 2nd Solution     IMPORTANT CONCEPT
 
+# SELECT D.Name AS Department ,E.Name AS Employee ,E.Salary 
+# from 
+# 	Employee E,
+# 	Department D 
+# WHERE E.DepartmentId = D.id 
+#   AND (DepartmentId,Salary) in 
+#   (SELECT DepartmentId,max(Salary) as max FROM Employee GROUP BY DepartmentId)
       
