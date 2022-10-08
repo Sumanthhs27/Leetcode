e381134449823d33f76d7deaf8b97b72721373b8
Sumanth H S <81132020+Sumanthhs27@users.noreply.github.com>
2022-10-09 01:39:08 +0530

    Time: 750 ms (69.77%), Space: 135.2 MB (89.16%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
@@ -6,14 +6,18 @@ public:
         if(n%2 != 0) return res;
         sort(arr.begin(), arr.end());
         unordered_map<int,int> m;
+        
         for(int i:arr){
             if(i%2 == 1) m[i]++;
             else if(m.find(i/2) == m.end()) m[i]++;
             else if(m[i/2] > 0) {res.push_back(i/2); m[i/2]--;}
             else m[i]++;
         }
+        
         vector<int> dummy;
         if(res.size() != n/2) return dummy;
         return res;
     }
 };
+
+// Sort the array, begin from smallest element, as array elements are increasing we will check if we have arr[i]/2 in the map, if yes then decrease its freq in map, if no add its freq.
