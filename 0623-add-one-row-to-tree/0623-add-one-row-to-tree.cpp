08e52644cb37ae8cf82e2ddc3c9ab92082b630b4
Sumanth H S <81132020+Sumanthhs27@users.noreply.github.com>
2022-10-19 19:42:16 +0530

    Time: 32 ms (69.27%), Space: 24.9 MB (84.75%) - LeetHub

---
+++ 0623-add-one-row-to-tree/0623-add-one-row-to-tree.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+void dfs(TreeNode* & r, int height, int& val, int& depth){
+    if(!r) return;
+    if(height == depth){
+        TreeNode* temp1 = new TreeNode(val);
+        temp1->left = r->left; temp1->right = NULL;
+        TreeNode* temp2 = new TreeNode(val);
+        temp2->left = NULL; temp2->right = r->right;
+        r->left = temp1;
+        r->right = temp2;
+        return;
+    }
+    dfs(r->left,height+1,val,depth);
+    dfs(r->right,height+1,val,depth);
+    return;
+}
+
+
+class Solution {
+public:
+    TreeNode* addOneRow(TreeNode* root, int val, int depth) {
+        if(depth == 1){
+            TreeNode* temp = new TreeNode(val);
+            temp->left = root;
+            temp->right = NULL;
+            root = temp;
+            return root;
+        }
+        
+        int height = 0;
+        depth -= 2;
+        dfs(root,height,val,depth);
+        return root;
+    }
+};
