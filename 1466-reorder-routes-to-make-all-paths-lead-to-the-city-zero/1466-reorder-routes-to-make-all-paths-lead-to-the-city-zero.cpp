2653b12fcf5c05be3962ae09dd56609efe05aa11
Sumanth H S <81132020+Sumanthhs27@users.noreply.github.com>
2022-10-20 15:34:02 +0530

    Time: 349 ms (98.96%), Space: 106.2 MB (74.65%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -2,38 +2,54 @@ class Solution {
 public:
     
     
-    void dfs(int& i,int& cnt,vector<int>& vis,vector<vector<int>> &adj,set<pair<int,int>>&s){
-        vis[i] = 1;
-        for(auto j : adj[i]){
-            if(!vis[j] && s.find({i,j}) != s.end()) cnt++;
-            if(!vis[j]) dfs(j,cnt,vis,adj,s);
-        }   
-    }
+//     void dfs(int& i,int& cnt,vector<int>& vis,vector<vector<int>> &adj,set<pair<int,int>>&s){
+//         vis[i] = 1;
+//         for(auto j : adj[i]){
+//             if(!vis[j] && s.find({i,j}) != s.end()) cnt++;
+//             if(!vis[j]) dfs(j,cnt,vis,adj,s);
+//         }   
+//     }
     
     
-    int minReorder(int n, vector<vector<int>>& conn) {
+//     int minReorder(int n, vector<vector<int>>& conn) {
         
-        set<pair<int,int>> s;
-        //vector<vector<int>> mat (n,vector<int> (n,0));
-        for(auto vec : conn){
-            int i = vec[0], j = vec[1];
-            s.insert({i,j});
-        }
-        vector<int> vis (n,0);
-        vector<vector<int>> adj(n);
-        for(auto vec : conn){
-            int u = vec[0], v = vec[1];
-            adj[u].push_back(v);
-            adj[v].push_back(u);
-        }
-        int cnt = 0;
+//         set<pair<int,int>> s;
+//         //vector<vector<int>> mat (n,vector<int> (n,0));
+//         for(auto vec : conn){
+//             int i = vec[0], j = vec[1];
+//             s.insert({i,j});
+//         }
+//         vector<int> vis (n,0);
+//         vector<vector<int>> adj(n);
+//         for(auto vec : conn){
+//             int u = vec[0], v = vec[1];
+//             adj[u].push_back(v);
+//             adj[v].push_back(u);
+//         }
+//         int cnt = 0;
         
-        for(int i=0; i<n; i++){
-            if(!vis[i]){
-                dfs(i,cnt,vis,adj,s);
-            }
-        }  
+//         for(int i=0; i<n; i++){
+//             if(!vis[i]){
+//                 dfs(i,cnt,vis,adj,s);
+//             }
+//         }  
         
-        return cnt;
+//         return cnt;
+//     }
+    int dfs(vector<vector<int>> &al, vector<bool> &visited, int from) {
+    auto change = 0;
+    visited[from] = true;
+    for (auto to : al[from])
+        if (!visited[abs(to)])
+            change += dfs(al, visited, abs(to)) + (to > 0);
+    return change;        
+}
+int minReorder(int n, vector<vector<int>>& connections) {
+    vector<vector<int>> al(n);
+    for (auto &c : connections) {
+        al[c[0]].push_back(c[1]);
+        al[c[1]].push_back(-c[0]);
     }
+    return dfs(al, vector<bool>(n) = {}, 0);
+}
 };
