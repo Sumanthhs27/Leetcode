e23b20c7e3b10843074f7a9800fb0bd4a52eb041
Sumanth H S <81132020+Sumanthhs27@users.noreply.github.com>
2024-10-29 14:20:47 +0530

    Time: 54 ms (39.33%), Space: 16.4 MB (99.37%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
@@ -1,27 +1,35 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         
-        cnt = 1
-        n = len(nums)
+        # cnt = 1
+        # n = len(nums)
 
-        idx = 0
-        res = 1
+        # idx = 0
+        # res = 1
 
-        for i in range(1,n):
-            if nums[i] != nums[idx]:
-                idx += 1
-                nums[idx], nums[i] = nums[i], nums[idx]
-                print(nums)
-                res += 1
-                cnt = 1
+        # for i in range(1,n):
+        #     if nums[i] != nums[idx]:
+        #         idx += 1
+        #         nums[idx], nums[i] = nums[i], nums[idx]
+        #         print(nums)
+        #         res += 1
+        #         cnt = 1
             
-            else:
-                if cnt < 2:
-                    res += 1
-                    idx += 1
-                    cnt += 1
-                    nums[idx], nums[i] = nums[i], nums[idx]
-        return res
-                
+        #     else:
+        #         if cnt < 2:
+        #             res += 1
+        #             idx += 1
+        #             cnt += 1
+        #             nums[idx], nums[i] = nums[i], nums[idx]
+        # return res
 
+        if len(nums) < 2: return len(nums)
+        slow, fast = 2, 2
+
+        while fast < len(nums):
+            if nums[slow - 2] != nums[fast]:
+                nums[slow] = nums[fast]
+                slow += 1
+            fast += 1
+        return slow
 
