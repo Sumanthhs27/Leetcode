b38ceff459cb1c223e099b00b13c680e054d894a
Sumanth H S <81132020+Sumanthhs27@users.noreply.github.com>
2025-03-04 22:32:35 +0530

    Time: 69 ms (12.92%), Space: 38.4 MB (7.42%) - LeetHub

---
+++ 3094-minimum-number-of-operations-to-make-array-empty/3094-minimum-number-of-operations-to-make-array-empty.py
@@ -0,0 +1,29 @@
+from collections import Counter
+class Solution:
+
+    def is_possible(self,three_cnt, freq):
+        return (freq-3*three_cnt)%2 == 0
+
+    def minOperations(self, nums: List[int]) -> int:
+        
+        mp = Counter(nums)
+        cnt = 0
+        print(mp)
+        for num, freq in mp.items():
+            if freq%3 == 0:
+                cnt += freq//3
+                continue
+            else:
+                max_ = freq//3
+                found = False
+                print(f"freq: {freq}, max_: {max_}")
+                for i in range(max_, -1, -1):
+                    if self.is_possible(i, freq):
+                        two_cnt = (freq-3*i)//2
+                        cnt += i + two_cnt
+                        found = True
+                        break
+                
+                if not found:
+                    return -1
+        return cnt
